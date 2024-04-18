3d96e8aa6f97aac016f6dec4ca39d2e6ec7a7b68
Hector Gonzalez <hgonzalez542@g.rwu.edu>
2024-04-17 23:18:02 -0400

    working on debuggin

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Products from './components/Products';
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/Home" exact component={Home} />
-        <Route path="/About" component={About} />
-        <Route path="/Products" component={Products} />
-      </Routes>
-    </Router>
-  );
-};
+function App() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                </ul>
+
+                <hr />
+              <Routes>
+                <Route exact path="/home" component={<Home />} />
+                <Route path="/about" component={<About />} />
+                <Route path="/products" component={<Products />} />
+              </Routes>
+            </div>
+        </Router>
+    );
+}
 
 export default App;
